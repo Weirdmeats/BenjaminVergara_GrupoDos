965654e700c66d0a8d6678a2fe45369554142462
Weirdmeats <165041244+Weirdmeats@users.noreply.github.com>
2024-11-04 21:28:36 -0300

    Version 4.2 Proyecto 04/11/2024
    Modificacion del cart.jsx

--- src/components/Pages/Cart/Cart.jsx
+++ src/components/Pages/Cart/Cart.jsx
@@ -6,22 +6,25 @@ const Cart = () => {
     const { state } = useShoppingCart(); 
 
     return (
-        <div className="container mx-auto">
+        <div className="bg-slate-200 py-10 px-4">
             <h1 className="text-2xl font-bold">Carrito de Compras</h1>
-            {/* Verifica si el carrito está vacío */}
+            {/*verifica si el carrito esta vacío */}
             {state.cart.length === 0 ? (
                 <p>No hay productos en el carrito.</p>
             ) : (
                 <ul>
-                    {/* Muestra la lista de productos en el carrito */}
+                    {/*muestra la lista de productos en el carrito */}
                     {state.cart.map((item, index) => (
                         <li key={index} className="flex justify-between border-b py-2">
                             <span>{item.name} (x{item.quantity})</span>
                             <span>${item.price * item.quantity}</span> 
                         </li>
                     ))}
+                    
                 </ul>
+                
             )}
+             <button className='bg-yellow-500 px-4 py-3 rounded-lg'>PAGAR COMPRA</button>
         </div>
     );
 };
