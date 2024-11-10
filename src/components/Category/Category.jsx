2a66488883fc7f820c29ded2560246920c37fb9d
Weirdmeats <165041244+Weirdmeats@users.noreply.github.com>
2024-11-10 16:13:34 -0300

    Version 5.1 Proyecto React+Vite 10/11/2024
    QUITANDO CODIGO DE MAS QUE SAQUE DE YOUTUBE.

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Category({ selectedCategory, setSelectedCategory }) {
     return (
         <div className="flex justify-center mb-8">
-            {['all', 'celulares', 'powerbanks', 'cargadores'].map(category => (
+            {['All', 'celulares', 'powerbanks', 'cargadores'].map(category => (
                 <button
                     key={category}
                     className={`px-4 py-2 mx-2 rounded transition duration-300 ${selectedCategory === category ? 'bg-slate-600 text-white' : 'bg-white text-slate-600 border border-slate-600 hover:bg-slate-100'}`}
